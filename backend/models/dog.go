7c293d814ba694b18c2f348662839840eacb0629
Bell <dante.be@hotmail.com>
2025-04-04 15:07:08 -0300

    Modificaciones en ambas estructuras de los modelos

--- backend/models/dog.go
+++ backend/models/dog.go
@@ -1,12 +1,31 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Dog struct {
 	gorm.Model
-	UserId      uint   `json:"user_id"`
-	Description string `json:"description"`
-	ImageUrl    string `json:"image_url"`
-	Location    string `json:"location"`
-	Status      string `json:"status"`
+	UserId            uint      `json:"user_id"`
+	Description       string    `json:"description"`
+	ImageUrl          string    `json:"image_url"`
+	Location          string    `json:"location"`
+	Status            bool      `json:"status"`
+	Nombre            string    `json:"nombre"`
+	RazaId            bool      `json:"raza"`
+	Tamanio           int8      `json:"tamanio"`
+	Color_Principal   int       `json:"color_principal"`
+	Color_Secundario  int       `json:"color_secundario"`
+	Color_Tercero     int       `json:"color_tercero"`
+	Dir               string    `string:"dir"`
+	FechaSucesi       time.Time `json:"Fecha"`
+	Tipo_Mascota      int8      `json:"TipoMascota"`
+	Sexo              int8      `json:"sexo"`
+	Estado            int8      `json:"estado"`
+	Castrado          bool      `json:"castrado"`
+	Sociable_Perros   bool      `json:"sociablePerro"`
+	Sociable_Personas bool      `json:"sociablePersona"`
+	UbicacionActual   string    `json:"UbicacionActual"`
 }
--- backend/models/user.go
+++ backend/models/user.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
+	Nombre   string `json:"nombre"`
 	Email    string `json:"email" gorm:"unique"`
 	Password string `json:"password"`
 }
